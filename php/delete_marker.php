09af73ffc0320c84c8d695888214ed30464a579a
bogdanioanilie <43874519+bogdanioanilie@users.noreply.github.com>
2019-01-09 21:07:54 +0200

    v1.3

--- php/delete_marker.php
+++ php/delete_marker.php
@@ -1,13 +1,18 @@
 <?php
+$admin = $_POST['admin'];
+$name  = $_POST['name'];
    $conn=mysqli_connect("localhost","root","pwdpwd","tim_users") or die(mysqli_error());
    $result = mysqli_query($conn,"SELECT * FROM `markers`");
  
- 
-		 $sql="DELETE FROM markers WHERE id='$id'";
+    if($admin == 1){
+		 $sql="DELETE FROM markers WHERE name='$name'";
     if(mysqli_query($conn, $sql)){
 		echo "Records were deleted successfully.";
 	} else{
 		echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+	}else{
+		echo "The command is not allowed.You don't have admin account");
+	}
 }
 
 ?>
